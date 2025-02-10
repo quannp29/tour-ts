7c36a22136af9e2be16980dc2baef35574a62a0b
Nguyen Phu Quan <93207485+npquan29@users.noreply.github.com>
2025-02-10 22:46:59 +0700

    get list tour with category

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -1,19 +1,41 @@
 import { Request, Response } from "express";
-import Tour from "../../models/tour.model";
+import sequelize from "../../config/database";
+import { QueryTypes } from "sequelize";
 
-// [GET] /tours
+// [GET] /tours:slugCategory
 export const index = async (req: Request, res: Response) => {
-  // SELECT * FROM tours WHERE deleted = false AND status = "active";
-  const tours = await Tour.findAll({
-    where: {
-      deleted: false,
-      status: "active"
-    },
-    raw: true
+  const slugCategory = req.params.slugCategory;
+
+  const tours = await sequelize.query(
+    `
+    SELECT tours.*, ROUND(price * (1 - discount/100), 0) AS price_special
+    FROM tours
+    JOIN tours_categories ON tours.id = tours_categories.tour_id
+    JOIN categories ON tours_categories.category_id = categories.id
+    WHERE
+      categories.slug = "${slugCategory}"
+      AND categories.deleted = false
+      AND categories.status = 'active'
+      AND tours.deleted = false
+      AND tours.status = 'active';
+  `,
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
+
+  tours.forEach((tour) => {
+    if (tour["images"]) {
+      const images = JSON.parse(tour["images"]);
+      tour["image"] = images[0];
+    }
+    tour["price_special"] = +tour["price_special"];
   });
+
+  console.log(tours);
 
   res.render("client/pages/tours/index", {
     pageTitle: "Danh sách tour",
-    tours: tours
+    tours: tours,
   });
-}
+};
