2de5b63258c54f6c46cb4f7c894bce467efee3e5
Nguyen Phu Quan <93207485+npquan29@users.noreply.github.com>
2025-02-10 22:22:44 +0700

    config layout website

--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -1,22 +1,8 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 const router: Router = Router();
 
-import Tour from "../../models/tour.model";
+import * as controller from "../../controllers/client/tour.controller";
 
-router.get("/", async (req: Request, res: Response) => {
-  // SELECT * FROM tours WHERE deleted = false AND status = "active";
-  const tours = await Tour.findAll({
-    where: {
-      deleted: false,
-      status: "active"
-    },
-    raw: true
-  });
-
-  res.render("client/pages/tours/index", {
-    pageTitle: "Danh sách tour",
-    tours: tours
-  });
-});
+router.get("/", controller.index);
 
 export const tourRoutes: Router = router;
