a5c5b083c36fd6138f15a9086fdbb134626e9432
Nguyen Phu Quan <93207485+npquan29@users.noreply.github.com>
2025-02-12 10:20:53 +0700

    create tour with upload many images

--- routes/admin/tour.route.ts
+++ routes/admin/tour.route.ts
@@ -1,12 +1,26 @@
 import { Router } from "express";
 const router: Router = Router();
+import multer from "multer";
 
 import * as controller from "../../controllers/admin/tour.controller";
+import * as uploadCloud from "../../middlewares/admin/uploadCloud.middleware";
+
+const upload = multer();
 
 router.get("/", controller.index);
 
 router.get("/create", controller.create);
 
-router.post("/create", controller.createPost);
+router.post(
+  "/create",
+  upload.fields([
+    {
+      name: "images",
+      maxCount: 10,
+    },
+  ]),
+  uploadCloud.uploadFields,
+  controller.createPost
+);
 
 export const tourRoutes: Router = router;
